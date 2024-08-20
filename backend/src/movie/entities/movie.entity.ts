2774b1d1f3cc5d3a5c762f4c1db99ff4fd5ee3cf
deivydas.kim <deivydas.kim@gmail.com>
2024-08-20 23:11:22 +0300

    refactor(entities): user and movie entities adjusted with real data

--- backend/src/movie/entities/movie.entity.ts
+++ backend/src/movie/entities/movie.entity.ts
@@ -1,1 +1,20 @@
-export class Movie {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+@Entity('movies')
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  releaseDate: Date;
+
+  @ManyToOne(() => User, (user) => user.movies, { onDelete: 'CASCADE' })
+  user: User;
+}
--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Movie } from '../../movie/entities/movie.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Movie, (movie) => movie.user, { cascade: ['remove'] })
+  movies: Movie[];
+}
