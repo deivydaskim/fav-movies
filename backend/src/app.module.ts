59e7d47a15c833d0ca5f0976e0b9fe9948555b5d
deivydas.kim <deivydas.kim@gmail.com>
2024-08-21 01:05:36 +0300

    refactor(app) remove unnecessary files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MovieModule } from './movie/movie.module';
 import { AuthModule } from './auth/auth.module';
@@ -20,10 +18,8 @@ import authConfig from './configs/auth.config';
       inject: [ConfigService],
     }),
     UserModule,
-    MovieModule,
     AuthModule,
+    MovieModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
